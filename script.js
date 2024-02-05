f063373db560cdf0afb7544ab12701d6640a8bff
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-05 01:17:58 -0300

    Code refactoring

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const switchButton = document
   .getElementById("switch")
   .addEventListener("click", () => {
-    var element = document.getElementById("body");
-    element.classList.toggle("light");
+    var body = document.getElementById("body");
+    body.classList.toggle("light");
   });
